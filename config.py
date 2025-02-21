9f3de814a95930ce2df4bf6569fda7401061b170
sangeethapriyaa26 <sangeetha.thavam@tigeranalytics.com>
2025-02-21 12:53:42 +0530

    updated

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ load_dotenv() #loading the env variables defined in .env file
 
 API_KEY = os.getenv('OPENWEATHERMAP_API_KEY') #to get the apikey stored in .env file
 
+
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather" #base url to which we'll append the specific city names
